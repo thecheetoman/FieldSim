3493fc1bdac3392a8b0889f1d8becae2b9cdfd08
sirbobisherenow <chetan.likes.nature@gmail.com>
2026-07-30 10:07:50 -0700

    Kitbot with working drivebase!

---
+++ Assets/Scripts/TankDrive.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TankDrive : MonoBehaviour
+{
+    private Vector2 movementInput; //Store movement input, updated through onMove method
+    public Rigidbody rigid;
+    public WheelCollider FL, FR, ML, MR, BL, BR;
+    public float drivespeed, rotationspeed;
+
+
+    public void OnMove(InputValue value)
+    {
+        movementInput = value.Get<Vector2>();
+        // get movement input, store it in movementinput var which is vector 2(x and y axis)
+    }
+    void FixedUpdate()
+    {
+        float motor = movementInput.y * drivespeed; // get the y axis of movement input and multiply it by drivespeed
+        float leftSideTorque = (movementInput.y * drivespeed) + (movementInput.x * rotationspeed);
+        float rightSideTorque = (movementInput.y * drivespeed) - (movementInput.x * rotationspeed);
+
+        FL.motorTorque = leftSideTorque;
+        ML.motorTorque = leftSideTorque;
+        BL.motorTorque = leftSideTorque;
+
+        FR.motorTorque = rightSideTorque;
+        MR.motorTorque = rightSideTorque;
+        BR.motorTorque = rightSideTorque;
+
+    }
+}
---
+++ Assets/Scripts/TankWheel.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TankWheel : MonoBehaviour
+{
+    public WheelCollider wheelCollider;
+    public Transform wheelMesh;
+
+    void Update()
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+
+        wheelMesh.position = pos;
+        // Multiply by your mesh's default local rotation offset (e.g., 90 degrees on Y or Z)
+        wheelMesh.Rotate(wheelCollider.rpm / 60f * 360f * Time.deltaTime, 0f, 0f, Space.Self);
+    }
+}
